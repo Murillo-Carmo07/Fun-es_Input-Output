8b788f289f8f8f875e776f695d375e53d2b45822
Murillo do Carmo Gonçalves <murillodecarmo@gmail.com>
2024-11-23 17:02:33 -0300

    Add files via upload

---
+++ atv6.py
@@ -0,0 +1,82 @@
+# 1 Imprima uma mensagem de boas-vindas na tela.
+mensagem = 'Boas Vindas!'
+print(mensagem)
+
+# 2 Declare uma variável booleana verdadeiro com o valor True e imprima seu tipo.
+num = bool(7)
+print(num)
+
+# 3 Imprima o resultado da multiplicação de dois números decimais de sua escolha
+num1 = 10
+num2 = 12
+
+mult = (num1*num2)
+print(mult)
+
+# 4 Imprima o resultado da divisão de dois números inteiros de sua escolha.
+num3 = 10
+num4 = 12
+
+div = (num3 / num4)
+print(div)
+
+# 5 Imprima o resultado da subtração de dois números inteiros de sua escolha
+num5 = 10
+num6 = 12
+
+sub = (num5 - num6)
+print(sub)
+
+# 6 Imprima o resultado da divisão inteira de dois números inteiros de sua escolha.
+num7 = 7
+num8 = 2
+
+div_inteira = (num7 // num8)
+print(div_inteira)
+
+# 7 Imprima o resultado da multiplicação de 4 números decimais de sua escolha.
+num9 = 10
+num10 = 10 
+num11 = 10
+num12 = 5
+
+mult = (num9*num10*num11*num12)
+print(mult)
+
+# 8 Declare uma variável numero e atribua um número inteiro. Em seguida,imprima o dobro desse número.
+num13 = 3
+
+dobro = num13 **2
+print(dobro)
+
+# 9 Crie uma calculadora de soma com as ferramentas que vc já conhece(Use apenas input, print e variavel).
+print('Calculadora')
+
+numero = int(input('numero1:'))
+numero2= int(input('numero2:'))
+
+resultado= print(numero + numero2)
+
+# 10 Crie um sistema de cadastro com as estruturas que vc já conhece(Use apenas input, print e variavel)
+Nome = str(input('Digite seu nome:'))
+Idade = int(input('Digite sua idade:')) 
+Gmail = str(input('Digite seu E-mail:'))
+NumeroT = str(input('Digite seu número de telefone:'))
+completo = print('Cadastro realizado com sucesso.')
+
+
+
+
+
+
+           
+
+
+
+
+
+
+
+
+
+
